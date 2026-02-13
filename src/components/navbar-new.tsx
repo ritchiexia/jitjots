4181abf571aac8686eacf6cf3180339428c41761
Derek Cheng <dca130@sfu.ca>
2026-02-12 23:54:06 -0800

    Update navbar-new.tsx

--- src/components/navbar-new.tsx
+++ src/components/navbar-new.tsx
@@ -58,6 +58,8 @@ export default function NewNavBar() {
                     <SheetContent side="right">
                         <div className="grid gap-2 py-6">
                             <Accordion type="single" collapsible>
+
+                                {/* Home Mobile */}
                                 <AccordionItem value="item-1" className="no-underline font-bold text-base hover:no-underline">
                                     <SheetClose asChild>
                                         <Link href="/" className={cn(
@@ -68,6 +70,8 @@ export default function NewNavBar() {
                                         </Link>
                                     </SheetClose>
                                 </AccordionItem>
+
+                                {/* About Mobile */}
                                 <AccordionItem value="item-2">
                                     <AccordionTrigger className={cn(
                                         ibmPlexMono.className,
@@ -79,13 +83,25 @@ export default function NewNavBar() {
                                                 About Us
                                             </Link>
                                         </SheetClose>
-                                        <SheetClose asChild>
-                                            <Link href="/volunteers" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                                Volunteer
+                                        {/* <SheetClose asChild>
+                                            <Link href="/mission" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Our Mission
                                             </Link>
                                         </SheetClose>
+                                        <SheetClose asChild>
+                                            <Link href="/meet-the-team" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Meet the Team
+                                            </Link>
+                                        </SheetClose>
+                                        <SheetClose asChild>
+                                            <Link href="/partners" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Our Partners
+                                            </Link>
+                                        </SheetClose> */}
                                     </AccordionContent>
                                 </AccordionItem>
+
+                                {/* Events Mobile */}
                                 <AccordionItem value="item-3">
                                     <AccordionTrigger className={cn(
                                         ibmPlexMono.className,
@@ -98,13 +114,33 @@ export default function NewNavBar() {
                                             </Link>
                                         </SheetClose>
                                         <SheetClose asChild>
+                                            <Link href="/worksheets" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Worksheets
+                                            </Link>
+                                        </SheetClose>
+                                        <SheetClose asChild>
                                             <Link href="/jot-your-path" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                                 Jot Your Path
                                             </Link>
                                         </SheetClose>
+                                    </AccordionContent>
+                                </AccordionItem>
+
+                                {/* Get Involved Mobile */}
+                                <AccordionItem value="item-4">
+                                    <AccordionTrigger className={cn(
+                                        ibmPlexMono.className,
+                                        "no-underline font-bold text-base hover:no-underline"
+                                    )}>GET INVOLVED</AccordionTrigger>
+                                    <AccordionContent>
                                         <SheetClose asChild>
-                                            <Link href="/worksheets" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                                Worksheets
+                                            <Link href="/volunteers" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Volunteers
+                                            </Link>
+                                        </SheetClose>
+                                        <SheetClose asChild>
+                                            <Link href="/contact-us" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                Contact Us
                                             </Link>
                                         </SheetClose>
                                     </AccordionContent>
@@ -114,6 +150,8 @@ export default function NewNavBar() {
                     </SheetContent>
                 </Sheet>
             </div>
+
+            {/* Desktop Nav */}
             <div className="hidden lg:flex items-center justify-between w-full p-4">
                 <Link href="/" className="flex items-center">
                     <Image
@@ -127,7 +165,6 @@ export default function NewNavBar() {
                         JIT JOTS
                     </h1>
                 </Link>
-
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
@@ -135,6 +172,8 @@ export default function NewNavBar() {
                                 <Link href="/" className={cn(ibmPlexMono.className, "text-lg font-extrabold")}>HOME</Link>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
+
+                        {/* About Desktop Nav */}
                         <NavigationMenuItem>
                             <NavigationMenuTrigger className={cn(
                                 ibmPlexMono.className,
@@ -145,10 +184,10 @@ export default function NewNavBar() {
                                     <li>
                                         <NavigationMenuLink asChild>
                                             <Link href="/about" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                                <div className="text-sm font-medium leading-none">About Us</div>
-                                                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                <div className="text-sm font-medium leading-none">Meet the Team</div>
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                                                     Meet Our Team.
-                                                </p>
+                                                </p> */}
                                             </Link>
                                         </NavigationMenuLink>
                                     </li>
@@ -156,15 +195,17 @@ export default function NewNavBar() {
                                         <NavigationMenuLink asChild>
                                             <Link href="/volunteers" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                                 <div className="text-sm font-medium leading-none">Volunteer</div>
-                                                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                                                     Join our team and make a difference.
-                                                </p>
+                                                </p> */}
                                             </Link>
                                         </NavigationMenuLink>
                                     </li>
                                 </ul>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
+
+                        {/* Events Desktop Nav */}
                         <NavigationMenuItem>
                             <NavigationMenuTrigger className={cn(
                                 ibmPlexMono.className,
@@ -176,9 +217,9 @@ export default function NewNavBar() {
                                         <NavigationMenuLink asChild>
                                             <Link href="/workshops" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                                 <div className="text-sm font-medium leading-none">Workshops</div>
-                                                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                                                     Book a workshop.
-                                                </p>
+                                                </p> */}
                                             </Link>
                                         </NavigationMenuLink>
                                     </li>
@@ -186,9 +227,9 @@ export default function NewNavBar() {
                                         <NavigationMenuLink asChild>
                                             <Link href="/jot-your-path" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                                 <div className="text-sm font-medium leading-none">Jot Your Path</div>
-                                                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                                                     Join our career panel event.
-                                                </p>
+                                                </p> */}
                                             </Link>
                                         </NavigationMenuLink>
                                     </li>
@@ -196,9 +237,41 @@ export default function NewNavBar() {
                                         <NavigationMenuLink asChild>
                                             <Link href="/worksheets" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                                                 <div className="text-sm font-medium leading-none">Worksheets</div>
-                                                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                                                     Download free worksheets.
-                                                </p>
+                                                </p> */}
+                                            </Link>
+                                        </NavigationMenuLink>
+                                    </li>
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+
+                        {/* Get Involved Desktop Nav */}
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger className={cn(
+                                ibmPlexMono.className,
+                                "flex w-full items-center py-2 text-lg font-bold"
+                            )}>GET INVOLVED</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="grid w-[400px] gap-3 p-6">
+                                    <li>
+                                        <NavigationMenuLink asChild>
+                                            <Link href="/about" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                <div className="text-sm font-medium leading-none">Volunteers</div>
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                    Meet Our Team.
+                                                </p> */}
+                                            </Link>
+                                        </NavigationMenuLink>
+                                    </li>
+                                    <li>
+                                        <NavigationMenuLink asChild>
+                                            <Link href="/volunteers" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                                                <div className="text-sm font-medium leading-none">Contact Us</div>
+                                                {/* <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                    Join our team and make a difference.
+                                                </p> */}
                                             </Link>
                                         </NavigationMenuLink>
                                     </li>
